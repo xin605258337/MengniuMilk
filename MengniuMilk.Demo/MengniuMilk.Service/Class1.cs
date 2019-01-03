f91eb8d7742e328e0dfd55552af2d750b9cf48b4
xinxin <605258337@qq.com>
2019-01-03 10:07:34 +0800

    1.3.01

---
+++ MengniuMilk.Demo/MengniuMilk.Service/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MengniuMilk.Service
+{
+    using MengniuMilk.Entity;
+    using MengniuMilk.IService;
+    public class Class1
+    {
+    }
+}
