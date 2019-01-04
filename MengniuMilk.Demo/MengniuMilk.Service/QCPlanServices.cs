fe079a984501369e615f00d172b445c297c62755
zhangjiashuo666 <45580351+zhangjiashuo666@users.noreply.github.com>
2019-01-04 10:58:26 +0800

    0104

--- MengniuMilk.Demo/MengniuMilk.Service/QCPlanServices.cs
+++ MengniuMilk.Demo/MengniuMilk.Service/QCPlanServices.cs
@@ -6,26 +6,18 @@ using System.Threading.Tasks;
 
 namespace MengniuMilk.Service
 {
+    using Dapper;
     using MengniuMilk.Entity;
     using MengniuMilk.IService;
     using Oracle.ManagedDataAccess.Client;
     using Oracle.ManagedDataAccess;
-    using System.Data;
-    using Newtonsoft.Json;
     using System.Configuration;
+    
 
     public class QCPlanServices : IQCPlanServices
     {
-        //连接数据库字符串
-        private static readonly string connStr = ConfigurationManager.ConnectionStrings["connString"].ConnectionString;
-        OracleConnection conn = null;
-        public QCPlanServices()
-        {
-            if (conn == null)
-            {
-                conn = new OracleConnection(connStr);
-            }
-        }
+       
+
         /// <summary>
         /// 新增质检计划
         /// </summary>
@@ -33,10 +25,13 @@ namespace MengniuMilk.Service
         /// <returns></returns>
         public int AddQCPlan(QCPlan qcPlan)
         {
-            string sql = string.Format("insert into QCPlan(Code,Name,Type_ID,ObjType_ID,Process_ID,Facility_ID,QCPlan_State,TargetType_ID,Target_ID,StandardValues,StandardValuesMax,StandardValuesMin) values(:Code,:Name,:Type_ID,:ObjType_ID,:Process_ID,:Facility_ID,:QCPlan_State,:TargetType_ID,:Target_ID,:StandardValues,:StandardValuesMax,:StandardValuesMin)",qcPlan.Code,qcPlan.Name,qcPlan.Type_ID,qcPlan.ObjType_ID,qcPlan.Process_ID, qcPlan.Facility_ID, qcPlan.QCPlan_State, qcPlan.TargetType_ID, qcPlan.Target_ID, qcPlan.StandardValues, qcPlan.StandardValuesMax, qcPlan.StandardValuesMin);
-           
-            int result = connForOracle.ExecuteSQL(sql);
-            return result;
+            using (OracleConnection conn = DapperHelper.GetConnString())
+            {
+                conn.Open();
+                string sql = @"insert into QCPlan(Code,Name,Type_ID,ObjType_ID,Process_ID,Facility_ID,QCPlan_State,TargetType_ID,Target_ID,StandardValues,StandardValuesMax,StandardValuesMin) values(:Code,:Name,:Type_ID,:ObjType_ID,:Process_ID,:Facility_ID,:QCPlan_State,:TargetType_ID,:Target_ID,:StandardValues,:StandardValuesMax,:StandardValuesMin)";
+                int result = conn.Execute(sql, qcPlan);
+                return result;
+            }
         }
 
         /// <summary>
@@ -46,9 +41,13 @@ namespace MengniuMilk.Service
         /// <returns></returns>
         public int DeleteQCPlan(int id)
         {
-            string sql = string.Format("delete from QCPlan where ID=:ID",id);
-            int result = connForOracle.ExecuteSQL(sql);
-            return result;
+            using (OracleConnection conn = DapperHelper.GetConnString())
+            {
+                conn.Open();
+                string sql = @"delete from QCPlan where ID=:ID";
+                var result = conn.Execute(sql, new { ID = id });
+                return result;
+            }
             
         }
 
@@ -58,9 +57,13 @@ namespace MengniuMilk.Service
         /// <returns></returns>
         public List<Facility> GetFacilities()
         {
-            string sql = "select * from Facility";
-            DataTable dt = connForOracle.ReturnDataSet(sql);
-            return JsonConvert.DeserializeObject<List<Facility>>(JsonConvert.SerializeObject(dt));
+            using (OracleConnection conn = DapperHelper.GetConnString())
+            {
+                conn.Open();
+                string sql = @"select * from Facility";
+                var result = conn.Query<Facility>(sql, null);
+                return result.ToList<Facility>();
+            }
         }
 
         /// <summary>
@@ -69,9 +72,14 @@ namespace MengniuMilk.Service
         /// <returns></returns>
         public List<Processes> GetProcess()
         {
-            string sql = "select * from Process";
-            DataTable dt = connForOracle.ReturnDataSet(sql);
-            return convert.ConvertToList<Processes>(dt);
+
+            using (OracleConnection conn = DapperHelper.GetConnString())
+            {
+                conn.Open();
+                string sql = @"select * from Processes";
+                var result = conn.Query<Processes>(sql, null);
+                return result.ToList<Processes>();
+            }
         }
         
         /// <summary>
@@ -80,9 +88,13 @@ namespace MengniuMilk.Service
         /// <returns></returns>
         public List<QCPlanObjType> GetQCPlanObjTypes()
         {
-            string sql = "select * from QCPlanObjType";
-            DataTable dt = connForOracle.ReturnDataSet(sql);
-            return JsonConvert.DeserializeObject<List<QCPlanObjType>>(JsonConvert.SerializeObject(dt));
+            using (OracleConnection conn = DapperHelper.GetConnString())
+            {
+                conn.Open();
+                string sql = @"select * from QCPlanObjType";
+                var result = conn.Query<QCPlanObjType>(sql, null);
+                return result.ToList<QCPlanObjType>();
+            }
         }
 
         /// <summary>
@@ -91,10 +103,13 @@ namespace MengniuMilk.Service
         /// <returns></returns>
         public List<QCPlan> GetQCPlans()
         {
-            string sql = "select q.id,q.code,q.name,t.type_name,p.process_name,o.objtype_name,f.facility_name,r.targettype_name,g.target_name,q.standardvalues,q.standardvaluesmax,q.standardvaluesmin from QCPlan q inner join QCPlanType t on q.type_id = t.type_id inner join Processes p on q.process_id = p.process_id inner join QCPlanObjType o on q.objtype_id = o.objtype_id inner join Facility f on q.facility_id = f.facility_id inner join TargetType r on q.targettype_id = r.targettype_id inner join Target g on q.target_id = g.target_id";
-            DataTable dt = connForOracle.ReturnDataSet(sql);
-            return JsonConvert.DeserializeObject<List<QCPlan>>(JsonConvert.SerializeObject(dt));
-            
+            using (OracleConnection conn = DapperHelper.GetConnString())
+            {
+                conn.Open();
+                string sql = "select q.id,q.code,q.name,t.type_name,p.process_name,o.objtype_name,f.facility_name,r.targettype_name,g.target_name,q.QCPlan_State,q.standardvalues,q.standardvaluesmax,q.standardvaluesmin from QCPlan q inner join QCPlanType t on q.type_id = t.type_id inner join Processes p on q.process_id = p.process_id inner join QCPlanObjType o on q.objtype_id = o.objtype_id inner join Facility f on q.facility_id = f.facility_id inner join TargetType r on q.targettype_id = r.targettype_id inner join Target g on q.target_id = g.target_id";
+                var result = conn.Query<QCPlan>(sql, null);
+                return result.ToList<QCPlan>();
+            }
         }
 
         /// <summary>
@@ -104,9 +119,13 @@ namespace MengniuMilk.Service
         /// <returns></returns>
         public List<QCPlan> GetQCPlansByID(int id)
         {
-            string sql =string.Format("select * from QCPlan where ID=:ID",id);
-            DataTable dt = connForOracle.ReturnDataSet(sql);
-            return JsonConvert.DeserializeObject<List<QCPlan>>(JsonConvert.SerializeObject(dt));
+            using (OracleConnection conn = DapperHelper.GetConnString())
+            {
+                conn.Open();
+                string sql = @"select * from QCPlan where ID=:ID";
+                var result = conn.Query<QCPlan>(sql, new { ID = id });
+                return result.ToList<QCPlan>();
+            }
 
 
         }
@@ -117,9 +136,13 @@ namespace MengniuMilk.Service
         /// <returns></returns>
         public List<QCPlanType> GetQCPlanTypes()
         {
-            string sql = "select * from QCPlanType";
-            DataTable dt = connForOracle.ReturnDataSet(sql);
-            return JsonConvert.DeserializeObject<List<QCPlanType>>(JsonConvert.SerializeObject(dt));
+            using (OracleConnection conn = DapperHelper.GetConnString())
+            {
+                conn.Open();
+                string sql = @"select * from QCPlanType";
+                var result = conn.Query<QCPlanType>(sql, null);
+                return result.ToList<QCPlanType>();
+            }
         }
 
         /// <summary>
@@ -128,9 +151,13 @@ namespace MengniuMilk.Service
         /// <returns></returns>
         public List<Target> GetTargets()
         {
-            string sql = "select * from Target";
-            DataTable dt = connForOracle.ReturnDataSet(sql);
-            return JsonConvert.DeserializeObject<List<Target>>(JsonConvert.SerializeObject(dt));
+            using (OracleConnection conn = DapperHelper.GetConnString())
+            {
+                conn.Open();
+                string sql = @"select * from Target";
+                var result = conn.Query<Target>(sql, null);
+                return result.ToList<Target>();
+            }
         }
 
         /// <summary>
@@ -139,9 +166,13 @@ namespace MengniuMilk.Service
         /// <returns></returns>
         public List<TargetType> GetTargetTypes()
         {
-            string sql = "select * from TargetType";
-            DataTable dt = connForOracle.ReturnDataSet(sql);
-            return JsonConvert.DeserializeObject<List<TargetType>>(JsonConvert.SerializeObject(dt));
+            using (OracleConnection conn = DapperHelper.GetConnString())
+            {
+                conn.Open();
+                string sql = @"select * from TargetType";
+                var result = conn.Query<TargetType>(sql, null);
+                return result.ToList<TargetType>();
+            }
         }
 
         /// <summary>
@@ -151,9 +182,13 @@ namespace MengniuMilk.Service
         /// <returns></returns>
         public int UpdateQCPlan(QCPlan qcPlan)
         {
-            string sql = string.Format("update QCPlan set Code=:Code,Name=:Name,Type_ID=:Type_ID,ObjType_ID=:ObjType_ID,Process_ID=:Process_ID,Facility_ID=:Facility_ID,QCPlan_State=:QCPlan_State,TargetType_ID=:TargetType_ID,Target_ID=:Target_ID,StandardValues=:StandardValues,StandardValuesMax=:StandardValuesMax,StandardValuesMin=:StandardValuesMin", qcPlan.Code, qcPlan.Name, qcPlan.Type_ID, qcPlan.ObjType_ID, qcPlan.Process_ID, qcPlan.Facility_ID, qcPlan.QCPlan_State, qcPlan.TargetType_ID, qcPlan.Target_ID, qcPlan.StandardValues, qcPlan.StandardValuesMax, qcPlan.StandardValuesMin);
-            var result = connForOracle.ExecuteSQL(sql);
-            return result;
+            using (OracleConnection conn = DapperHelper.GetConnString())
+            {
+                conn.Open();
+                string sql = @"update QCPlan set Code=:Code,Name=:Name,Type_ID=:Type_ID,ObjType_ID=:ObjType_ID,Process_ID=:Process_ID,Facility_ID=:Facility_ID,QCPlan_State=:QCPlan_State,TargetType_ID=:TargetType_ID,Target_ID=:Target_ID,StandardValues=:StandardValues,StandardValuesMax=:StandardValuesMax,StandardValuesMin=:StandardValuesMin";
+                var result = conn.Execute(sql, qcPlan);
+                return result;
+            }
         }
     }
 }
