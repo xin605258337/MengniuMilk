e4f381e4d26de984a07c1d6a003c3fc5ca812b57
YoonaZBJ <45481297+YoonaZBJ@users.noreply.github.com>
2019-01-05 14:19:08 +0800

    1.05-zbj02

---
+++ MengniuMilk.Demo/MengniuMilk.UI/Controllers/DataGatherController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MengniuMilk.UI.Controllers
+{
+    public class DataGatherController : Controller
+    {
+        // GET: DataGather
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
