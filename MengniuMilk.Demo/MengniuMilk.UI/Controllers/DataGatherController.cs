4a7762880d3dc23af8d5b071d952c1c34b6a7b8a
zhangjiashuo666 <45580351+zhangjiashuo666@users.noreply.github.com>
2019-01-06 19:38:59 +0800

    zjs16

--- MengniuMilk.Demo/MengniuMilk.UI/Controllers/DataGatherController.cs
+++ MengniuMilk.Demo/MengniuMilk.UI/Controllers/DataGatherController.cs
@@ -19,9 +19,6 @@ namespace MengniuMilk.UI.Controllers
             return View();
         }
 
-        public ActionResult UpdateDataGather()
-        {
-            return View();
-        }
+       
     }
 }
