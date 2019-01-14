a671a374bb9f2ffc4fba5583d751e1e51cb5385c
xinxin <605258337@qq.com>
2019-01-14 10:12:51 +0800

    1.14.02

---
+++ MengniuMilk.Demo/MengniuMilk.UI/Controllers/QCResultListController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MengniuMilk.UI.Controllers
+{
+    public class QCResultListController : Controller
+    {
+        // GET: QCResultList
+        public ActionResult Index()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 添加质检结果明细
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult AddQCResult()
+        {
+            return View();
+        }
+    }
+}
