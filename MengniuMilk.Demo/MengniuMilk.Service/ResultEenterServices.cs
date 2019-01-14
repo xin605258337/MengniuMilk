f5fe7d4180b64f6314c92f521aa2e4d34e592e4e
17937458081 <45580217+17937458081@users.noreply.github.com>
2019-01-14 18:54:14 +0800

    Update ResultEenterServices.cs

--- MengniuMilk.Demo/MengniuMilk.Service/ResultEenterServices.cs
+++ MengniuMilk.Demo/MengniuMilk.Service/ResultEenterServices.cs
@@ -6,7 +6,27 @@ using System.Threading.Tasks;
 
 namespace MengniuMilk.Service
 {
-    class ResultEenter
+    using Dapper;
+    using MengniuMilk.Entity;
+    using MengniuMilk.IService;
+    using Oracle.ManagedDataAccess.Client;
+    using Oracle.ManagedDataAccess;
+    using System.Configuration;
+    public class ResultEenterServices : IResultEenterServices
     {
+        /// <summary>
+        ///质检结果录入日志
+        /// </summary>
+        /// <returns></returns>
+        public List<ResultEenter> GetResultEenters()
+        {
+            using (OracleConnection conn = DapperHelper.GetConnString())
+            {
+                conn.Open();
+                string sql = @"";
+                var result = conn.Query<ResultEenter>(sql, null);
+                return result.ToList<ResultEenter>();
+            }
+        }
     }
 }
