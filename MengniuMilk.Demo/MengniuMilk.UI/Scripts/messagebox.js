7c72cce35facb185655321c7a3d4d3ffe9b32b96
zhangjiashuo666 <45580351+zhangjiashuo666@users.noreply.github.com>
2019-01-18 18:37:18 +0800

    zjss

---
+++ MengniuMilk.Demo/MengniuMilk.UI/Scripts/messagebox.js
@@ -0,0 +1,8 @@
+﻿function showmessagebox(flag,message) {
+    if (flag) {
+        $(".messageBox").removeClass("messageError").text(message).css("mafrgin-left", -($(".messageBox").outerWidth() / 2)).animate({ top: 0 }, "fast").delay(1500).animate({ top: "-57px" }, "fast");  
+    }
+    else {
+        $(".messageBox").addClass("messageError").text(message).css("margin-left", -($(".messageBox").outerWidth() / 2)).animate({ top: 0 }, "fast").delay(1500).animate({ top: "-57px" }, "fast");     
+    }
+}
